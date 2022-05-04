fb86190efef5518cea2abbfa3e52bec1b59f9f4f
jagadeeshg0801 <jagadeeshg0801@gmail.com>
2022-05-04 17:52:42 +0530

    build max limit changes,  dyanmic star occupany value added

--- src/app/components/start-rating/start-rating.component.ts
+++ src/app/components/start-rating/start-rating.component.ts
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartRatingComponent implements OnInit {
 
+  public totalStars = 6;
+  public eachStarOccupancyRange = 100/this.totalStars;
   public ratings =[
     {
       name: 'jsh',
       rating: 4,
+      content: 'hello this sample content hello this sample content hello this sample content hello this sample content'
+    },{
+      name: 'ABC',
+      rating: 3.5,
       content: 'hello this sample content hello this sample content hello this sample content hello this sample content'
     },{
       name: 'Vsh',
