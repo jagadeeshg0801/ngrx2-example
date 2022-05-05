7c442421f9659d2992ed949d404e44ecaa88e7cb
jagadeeshg0801 <jagadeeshg0801@gmail.com>
2022-05-05 17:39:20 +0530

    Font-awesome element tags -implementation done

--- src/app/components/start-rating/start-rating.component.ts
+++ src/app/components/start-rating/start-rating.component.ts
@@ -9,7 +9,7 @@ export class StartRatingComponent implements OnInit {
 
   public totalStars = 5;
   public eachStarOccupancyRange = 100/this.totalStars;
-  public ratings =[
+  public ratingsForCss =[
     {
       name: 'jsh',
       rating: 4,
@@ -31,10 +31,31 @@ export class StartRatingComponent implements OnInit {
       rating: 2.5,
       content: 'hello this sample content hello this sample content hello this sample content hello this sample content'
     },
+  ];
+  isCssType: boolean = true;
+
+  // THis is for HTML icons implemnetation
+
+  public math: Math ;
+  ratingsForHTML = [{
+    name: 'jsh',
+    rating: 4,
+    content: 'hello this sample content hello this sample content hello this sample content hello this sample content'
+  }];
+  emptyratingIcons = [
+    {name: 's1'},
+    {name: 's2'},
+    {name: 's3'},
+    {name: 's4'},
+    {name: 's5'}
   ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onType(type:string){
+    this.isCssType = type ==='css'? true : false;
   }
 
 }
